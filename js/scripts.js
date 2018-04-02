ad2ff6de3251106e66c6d463222a7c995d67db1c
Magda Gutowska <m.gutowska0909@gmail.com>
2018-04-02 22:29:30 +0200

    Change script.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,20 @@ var a = prompt('Podaj bok a');
 var	h = prompt('Podaj wysokość h');
 	
 function getTriangleArea(a, h) {
-	if (a =< 0) {
-		alert("Nieprawidłowe dane");
-	}
-	if (h =< 0) {
+	if (a <= 0 || h <= 0) {
 		alert("Nieprawidłowe dane");
 	}
 
 	return a * h / 2;
 }
 
+console.log(getTriangleArea(a, h));
 console.log(getTriangleArea(10, 6));
 
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(13, 6);
+var triangle3Area = getTriangleArea(34, 18);
 
-
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
